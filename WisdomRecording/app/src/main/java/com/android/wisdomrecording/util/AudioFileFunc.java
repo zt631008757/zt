e41c6206121c99958c9c728f818e12fc52a13014
zt <631008757@qq.com>
2018-07-19 18:32:41 +0800

    2018-7-19 录音采样率

--- WisdomRecording/app/src/main/java/com/android/wisdomrecording/util/AudioFileFunc.java
+++ WisdomRecording/app/src/main/java/com/android/wisdomrecording/util/AudioFileFunc.java
@@ -17,7 +17,7 @@ public class AudioFileFunc {
 
     //采用频率
     //44100是目前的标准，但是某些设备仍然支持22050，16000，11025
-    public final static int AUDIO_SAMPLE_RATE = 44100;  //44.1KHz,普遍使用的频率
+    public final static int AUDIO_SAMPLE_RATE = 8000;  //44.1KHz,普遍使用的频率  44100
     //录音输出文件
     private final static String AUDIO_RAW_FILENAME = "RawAudio.raw";
     private final static String AUDIO_WAV_FILENAME = "FinalAudio.wav";
